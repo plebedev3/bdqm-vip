3b7ba1ee742c2c6aefde4bf12a23e140074a928b
Rahil Patel <rahilpatel99@gmail.com>
2020-04-16 07:04:26 -0400

    added example qsub

---
+++ scripts/Simple_NN/example.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+#PBS -l nodes=1:ppn=4:gpus=1
+#PBS -l walltime=8:00:00
+#PBS -q pace-ice-gpu
+#PBS -N bdqm-nn-1
+#PBS -o stdout1
+#PBS -e stderr1
+
+cd /scratch/
+module load git
+rm -rf bdqm-vip/
+git clone https://github.com/medford-group/bdqm-vip.git
+cd /scratch/bdqm-vip/scripts/Simple_NN/
+module load anaconda3/2019.03
+conda activate tf32
+python run.py
+cp LOG /nv/pace-ice/{gtusername}/jobs/outputs/output-default.log
